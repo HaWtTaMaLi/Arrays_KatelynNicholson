27c55680e312c03c963c0d61d95f634b89db24c8
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-09-17 14:16:09 -0300

    Added Party Levels

--- Program.cs
+++ Program.cs
@@ -10,13 +10,38 @@ namespace Arrays_KatelynNicholson
     {
         //elements of the array "element" 
         static string[] partyNames = { "Number1", "Number2", "Number3", "Number4" };
+        static int[] partyLevels = new int[4];
 
         static void Main()
         {
-            Console.WriteLine(partyNames[0]);
+            partyLevels[0] = 1;
+            partyLevels[1] = 1;
+            partyLevels[2] = 1;
+            partyLevels[3] = 1;
+
+            LevelUpPartyMember("Number2");
+            LevelUpPartyMember("Number2");
+            LevelUpPartyMember("Number2");
+            LevelUpPartyMember("Number3");
+            LevelUpPartyMember("Number3");
+            LevelUpPartyMember("Number3");
+            LevelUpPartyMember("Number4");
+
+            Console.WriteLine(partyLevels[0]);
+            Console.WriteLine(partyLevels[1]);
+            Console.WriteLine(partyLevels[2]);
+            Console.WriteLine(partyLevels[3]);
+            Console.WriteLine(partyLevels[4]);
 
             Console.ReadKey();
 
+        }
+
+        static void LevelUpPartyMember(string memberName)
+        {
+            int indextofPartyMember = Array.IndexOf(partyNames, memberName);
+
+            partyLevels[indextofPartyMember] += 1;
         }
     }
 }
