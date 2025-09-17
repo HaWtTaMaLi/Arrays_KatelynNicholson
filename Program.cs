0ac96120f1b23056c27cf732cc4557c1abd4bbda
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-09-17 13:53:21 -0300

    Made my First array

--- Program.cs
+++ Program.cs
@@ -8,8 +8,14 @@ namespace Arrays_KatelynNicholson
 {
     internal class Program
     {
+        //elements of the array "element" 
+        static string[] partyNames = { "Number1", "Number2", "Number3", "Number4" };
+
         static void Main()
         {
+            Console.WriteLine(partyNames[0]);
+
+            Console.ReadKey();
 
         }
     }
