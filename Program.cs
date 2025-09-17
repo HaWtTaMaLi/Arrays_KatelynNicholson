3a309965aea285de807fadf37299c215e1b8eabf
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-09-17 14:38:26 -0300

    breaking down what i need
    erased old code. Broke down what i need

--- Program.cs
+++ Program.cs
@@ -9,39 +9,22 @@ namespace Arrays_KatelynNicholson
     internal class Program
     {
         //elements of the array "element" 
-        static string[] partyNames = { "Number1", "Number2", "Number3", "Number4" };
-        static int[] partyLevels = new int[4];
+        //array for weaponNames
+        //array for weapin power
+        //add an weapon fire rate (shot/second)
 
         static void Main()
         {
-            partyLevels[0] = 1;
-            partyLevels[1] = 1;
-            partyLevels[2] = 1;
-            partyLevels[3] = 1;
-
-            LevelUpPartyMember("Number2");
-            LevelUpPartyMember("Number2");
-            LevelUpPartyMember("Number2");
-            LevelUpPartyMember("Number3");
-            LevelUpPartyMember("Number3");
-            LevelUpPartyMember("Number3");
-            LevelUpPartyMember("Number4");
-
-            Console.WriteLine(partyLevels[0]);
-            Console.WriteLine(partyLevels[1]);
-            Console.WriteLine(partyLevels[2]);
-            Console.WriteLine(partyLevels[3]);
-            Console.WriteLine(partyLevels[4]);
-
-            Console.ReadKey();
+            
+            //ammo must be gun specific
+            //firing and reloading only applied to current gun
 
         }
 
-        static void LevelUpPartyMember(string memberName)
-        {
-            int indextofPartyMember = Array.IndexOf(partyNames, memberName);
+        //show HUD
+        //fire 
+        //reload
+        //range checking
 
-            partyLevels[indextofPartyMember] += 1;
-        }
     }
 }
